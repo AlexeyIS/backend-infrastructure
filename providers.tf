f67886fa779156b7dd4ae0625f0fd90d9537b534
Mo Hajr <mo@hajr.io>
2023-06-02 20:11:16 +0300

    Adding S3 Backend

--- providers.tf
+++ providers.tf
@@ -1,10 +1,19 @@
 terraform {
   required_version = ">= 1.3"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "5.0.1"
     }
+  }
+
+  ###Variables not allowed within here so all the following values needs to be hardcoded
+  backend "s3" {
+    bucket  = "<prcreated-bucket-name>"
+    key     = "terraform.tfstate"
+    profile = "<same-as var.aws_profile>"
+    region  = "us-east-1"
   }
 }
 
